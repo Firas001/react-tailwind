aecfb8353f369fa5f8ff664dd5e3c34c93415d86
Firas Al Dweni <al.dweni001@gmail.com>
2023-01-02 00:27:49 +0200

    push first app

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="container m-10 mx-auto rounded-xl border bg-gray-200 p-8 shadow">
+      <p className="mb-5 text-3xl font-bold text-gray-700">Welcome!</p>
+      <p className="text-lg text-gray-500">React and Tailwind CSS in action</p>
+    </div>
+  );
+}
+
+export default App;
